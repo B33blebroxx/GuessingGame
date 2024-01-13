aa5da95e90da05d8daaac97d02ffdb9d166a16b1
B33blebroxx <b33blebroxx@gmail.com>
2024-01-13 12:07:40 -0600

    completed phases 1-5

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int userGuess = 0;
+Random random = new();
+int secretNumber = random.Next(0, 101);
+List<int> userGuesses = new();
+
+void GuessPrompt()
+{
+    Console.WriteLine("\n\t Guess the secret number!");
+    Console.Write($"\n\t\tMy guess(attempt {GuessesRemaining(userGuesses.Count)}): ");
+    userGuess = Convert.ToInt32(Console.ReadLine());
+
+    if (userGuess == secretNumber)
+    {
+        Console.WriteLine("\n\tCongratulations, you guessed correctly!");
+    }
+    else
+    {
+        Console.WriteLine($"\n\tSorry, you guessed incorrectly. You may try {GuessesRemaining(userGuesses.Count) - 1} more times.");
+        userGuesses.Add(userGuess);
+
+        if (userGuesses.Count >= 4)
+        {
+            Console.WriteLine("\n\t\tSorry, you have no tries left!");
+            Environment.Exit(0);
+        }
+        else
+        {
+            Console.Write("\n\n\tPlease try again...");
+            GuessPrompt();
+        }
+
+    }
+
+    int GuessesRemaining(int guesses)
+    {
+        int guessesRemaining = 0;
+        if (guesses < 4)
+        {
+            guessesRemaining = 4 - guesses;
+        }
+        return guessesRemaining;
+    }
+}
+
+    GuessPrompt();
